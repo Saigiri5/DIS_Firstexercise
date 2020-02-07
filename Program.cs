bfca6d391c1dac8453b7f96a8476583f0f824584
Girija Bandaru <girijabandaru@USF-Wifi-224-80-4.laptops.usf.edu>
2020-02-07 11:57:47 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace inclass
+{
+    class Car
+    {
+        string color;
+        string make;
+        public static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.color = "Blue";
+            car1.make = "Ford";
+
+            Car car2 = new Car();
+            car2.color = "Red";
+            car2.make = "Hyundai";
+
+            Console.WriteLine(car1.color + " " +car1.make);
+        }
+    }
+}
