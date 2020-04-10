5a77ea29a972c4f7603dcb7ae5cfbc16fa97f0d8
Girija Bandaru <girijabandaru@Girijas-MacBook-Air.local>
2020-04-10 12:20:16 -0400

    In class commit

---
+++ Routes_Filters_ModelBinding_Validation-master/Routes_Filters_ModelBinding_Validation/Models/project.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Routes_Filters_ModelBinding_Validation.Models
+{
+    public class project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string GroupName { get; set; }
+    }
+    
+}
---
+++ Routes_Filters_ModelBinding_Validation-master/Routes_Filters_ModelBinding_Validation/Views/ModelBinding/Projectname.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Routes_Filters_ModelBinding_Validation.Views.ModelBinding
+{
+    public class Projectname
+    {
+        public Projectname()
+        {
+        }
+    }
+}
